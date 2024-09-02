9f391adf1ff534cb3b3ee445836e1c5d0e9a4d84
EzraArya <ezrawijaya10@gmail.com>
2024-09-02 22:18:47 +0700

    feat: add styling

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer d-flex align-items-center justify-content-center">
+    <h5>Made by Ezra</h5>
+</div>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,12 +7,35 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Document</title>
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+        }
+        .wrapper {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+        .content {
+            flex: 1; 
+        }
+        .footer {
+            background-color: #f8f9fa; 
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    @include('components.header')
-    <div class="container m-4">
-        @yield('content')
+    <div class="wrapper">
+        @include('components.header') 
+        
+        <div class="content container m-4">
+            @yield('content') 
+        </div>
+        
+        @include('components.footer') 
     </div>
-    @include('components.footer')
 </body>
 </html>
